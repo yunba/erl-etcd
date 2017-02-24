e833c1e5eab945bb79dcda78e196c0306ba4abf4
zengda <zengda@yunba.io>
2017-02-24 11:21:43 +0800

    Add API document.

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 ## erl-etcd
 
 Erlang driver for etcd. Requried R16 or higher
+
+## Quick start
+
+```erlang
+%% start the application
+application:start(etcd).
+
+%% set a key
+etcd:set("/message", "hello").
+
+%% get a key/dir
+{ok, Body} = etcd:get("/message").
+
+%% delete a key/dir
+etcd:delete("/message").
+
+%% watch a key
+Callback = fun(V) -> io:format(V), ok end,
+etcd:watch("/message", Callback).
+
+%% watch a dir
+Callback = fun(V) -> io:format(V), ok end,
+etcd:watch_dir("/test_dir", Callback).
+
+
+```
+
+Refer to `src/etcd.erl` to see more details about APIs.
+
+## v0.1-alpha
+This is the first alpha version of etcd erlang client. Supporting:
+- Basic CURD
+- Basic wait operation
--- src/etcd.erl
+++ src/etcd.erl
@@ -2,26 +2,45 @@
 
 -export([set/3, set/2, get/1, delete/1, watch/2, watch_dir/2]).
 
--spec set(Key::list(), Value::list(), TTL::integer()) -> {ok, list()}.
+%%%% set up a key with value with a TTL value(in seconds).
+%%%% return is {ok, response string list from etcd}
+-spec set(Key::list(), Value::list(), TTL::integer()) -> {ok, list()}| {fail, Reason::atom()}.
 set(Key, Value, TTL) ->
     gen_server:call(etcd_worker, {set, Key, Value, TTL}).
 
--spec set(Key::list(), Value::list()) -> {ok, list()}.
+%%%% set up a key with value WITHOUT a TTL value.
+%%%% return is {ok, response string list from etcd}
+-spec set(Key::list(), Value::list()) -> {ok, list()} | {fail, Reason::atom()}.
 set(Key, Value ) ->
     gen_server:call(etcd_worker, {set, Key, Value}).
 
--spec get(Key::list()) -> {ok, list()}.
+%%%% get the value of a key/dir.
+%%%% return is {ok, response string list from etcd}
+%%%% if the key doesn't exist, return {fail, not_found}
+-spec get(Key::list()) -> {ok, list()}| {fail, Reason::atom()}.
 get(Key) ->
     gen_server:call(etcd_worker, {get, Key}).
 
--spec delete(Key::list()) -> {ok, list()}.
+%%%% delete the value of a key/dir.
+%%%% return is {ok, response string list from etcd}
+%%%% if the key doesn't exist, it will return {ok, _} as well.
+-spec delete(Key::list()) -> {ok, list()}| {fail, Reason::atom()}.
 delete(Key) ->
     gen_server:call(etcd_worker, {delete, Key}).
 
+%%% wait for the key changing event asynchronously
+%%% when the key is changed, Callback function will be called,
+%%% and the input will be the response string from etcd.
+%%% the Callback should return ok to continue waiting, or stop to exit the waiting.
+%%% Alarm: This API won't work for dir
 -spec watch(Key::list(), Callback::fun((list())->(ok|stop))) -> ok.
 watch(Key, Callback) ->
     gen_server:cast(etcd_worker, {watch, Key, undefined, Callback}).
 
+%%% Wait for the dir changing event asynchronously.
+%%% when the any key in the dir is changed, Callback function will be called,
+%%% and the input will be the response string from etcd.
+%%% the Callback should return ok to continue waiting, or stop to exit the waiting.
 -spec watch_dir(Key::list(), Callback::fun((list())->(ok|stop))) -> ok.
 watch_dir(Key, Callback) ->
     gen_server:cast(etcd_worker, {watch_dir, Key, undefined, Callback}).
